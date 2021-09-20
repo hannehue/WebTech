02a938fc77ca7d7502786e16e4a946e8f7a51ab9
Hannehue <hannehue@gmail.com>
2021-09-20 11:21:11 +0200

    Add button to input in songs

--- lec02/Web.js
+++ lec02/Web.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("#new-song-button").click(function () {
         console.log("yeet"); 
-        $("#album-list").append('<li><input class="song-input" type="text" name="songs" id="song-3"></li>');        
+        $("#album-list").append('<li><input class="song-input" type="text" name="songs"></li>');        
     });
 });
 
