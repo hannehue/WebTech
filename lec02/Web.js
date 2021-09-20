ec9643dacd034c10bb3e5cfb7ca4d014e5527b53
Hannehue <hannehue@gmail.com>
2021-09-20 15:28:07 +0200

    Disable/Enable buttons

--- lec02/Web.js
+++ lec02/Web.js
@@ -2,20 +2,26 @@ let songCount = 0;
 
 
 $(document).on('click', '#new-song-button', function () {
-    console.log("Add song")
-    if (songCount >= 19) {
+    console.log("Add song");
+    songCount++;
+    if (songCount >= 20) {
+        $(this).prop('disabled', true);
         console.log("Too many songs");
-    } else {
-        songCount++;
-        $("#album-list").append('<div class="song-input-item"><input class="song-input" type="text" name="songs"><button type="button" id="delete-song-button" class="delete-song-btn"><i class="fas fa-trash"></i></button></div>');
     }
+    $("#album-list").append('<div class="song-input-item"><input class="song-input" type="text" name="songs"><button type="button" id="delete-song-button" class="delete-song-btn"><i class="fas fa-trash"></i></button></div>');
+
+    if (songCount === 1) {
+        $('#delete-song-button').prop('disabled', true);
+    } else {
+        $('#delete-song-button').prop('disabled', false);
+    }
+
 });
-
-
 
 $(document).on('click', '#delete-song-button', function () {
     console.log('remove');
     $(this).closest('div').remove();
     songCount--;
+    $('#new-song-button').prop('disabled', false);
 });
 
