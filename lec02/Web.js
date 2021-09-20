6bf71114cee0afb4c01a80e61e9ea8d368145279
Hannehue <hannehue@gmail.com>
2021-09-20 14:54:14 +0200

    Add and remove song buttons

--- lec02/Web.js
+++ lec02/Web.js
@@ -1,10 +1,21 @@
-$(document).ready(function () {
-    $("#new-song-button").click(function () {
-        console.log("yeet"); 
-        $("#album-list").append('<li><input class="song-input" type="text" name="songs"></li>');        
-    });
+let songCount = 0;
+
+
+$(document).on('click', '#new-song-button', function () {
+    console.log("Add song")
+    if (songCount >= 19) {
+        console.log("Too many songs");
+    } else {
+        songCount++;
+        $("#album-list").append('<div class="song-input-item"><input class="song-input" type="text" name="songs"><button type="button" id="delete-song-button" class="delete-song-btn"><i class="fas fa-trash"></i></button></div>');
+    }
 });
 
 
 
+$(document).on('click', '#delete-song-button', function () {
+    console.log('remove');
+    $(this).closest('div').remove();
+    songCount--;
+});
 
