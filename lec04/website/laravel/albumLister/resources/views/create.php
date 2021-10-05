d0f02c910960e07b0c64a7b46710606f81255b7b
hannehue <hannehue@gmail.com>
2021-10-05 14:52:15 +0200

    Setup laravel edit

---
+++ lec04/website/laravel/albumLister/resources/views/create.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Created album</title>
+</head>
+<body>
+    <h1>Created new album: <?php echo $_POST['name'] ?></h1>
+
+    <div class="album-info">
+        <div class="album-item">
+            <label>Album name: <?php echo $_POST['name'] ?></label>
+        </div>
+        <div class="album-item">
+            <label>Album release year: <?php echo $_POST['album-release'] ?></label>
+        </div>
+        <div class="album-item">
+            <label>Artist: <?php echo $_POST['artist'] ?></label>
+        </div>
+        <div class="album-item">
+            <label>Album description: <?php echo $_POST['album-description'] ?></label>
+        </div>
+        <div class="album-item">
+            <label>Number of songs: <?php echo count($_POST['song']);?></label>
+        </div>
+
+
+    </div>
+    
+</body>
+</html>
--- lec04/website/laravel/albumLister/routes/web.php
+++ lec04/website/laravel/albumLister/routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/new-album.php', function () {
+    return view('new-album');
+});
+
+Route::get('/create.php', function () {
+    return view('create');
 });
